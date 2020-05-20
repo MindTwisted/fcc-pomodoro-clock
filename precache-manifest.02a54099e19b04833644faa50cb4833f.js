7e16a77ad22e9f4fd1b31fe35b8e31d7e1986a66
Moiseienko Mykola <mmoiseienko@gfl.geeksforless.net>
2020-05-20 18:33:16 +0300

    Updates

---
+++ precache-manifest.02a54099e19b04833644faa50cb4833f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176f13c3408532ae00e13284164d0fba",
+    "url": "https://mindtwisted.github.io/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "eb17ea5b28c176fde85d",
+    "url": "https://mindtwisted.github.io/fcc-pomodoro-clock/static/css/main.428ebdab.chunk.css"
+  },
+  {
+    "revision": "505e064b66bf5f970d67",
+    "url": "https://mindtwisted.github.io/fcc-pomodoro-clock/static/js/2.ca851416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://mindtwisted.github.io/fcc-pomodoro-clock/static/js/2.ca851416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb17ea5b28c176fde85d",
+    "url": "https://mindtwisted.github.io/fcc-pomodoro-clock/static/js/main.fc477adc.chunk.js"
+  },
+  {
+    "revision": "4a6bd297a56b3ed9c4d9",
+    "url": "https://mindtwisted.github.io/fcc-pomodoro-clock/static/js/runtime-main.bca0ca46.js"
+  },
+  {
+    "revision": "988833bf34d92eba3d536029cd9e1bc6",
+    "url": "https://mindtwisted.github.io/fcc-pomodoro-clock/static/media/BeepSound.988833bf.wav"
+  }
+]);
